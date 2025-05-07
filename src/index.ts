09e20e088dd93f9599726d5b504c212367294c1f
dhayax <dhayanithi756@gmail.com>
2025-05-07 20:31:58 +0530

    added the functionality to avoid sending messages to the disconnected sockets

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-import { WebSocketServer,WebSocket } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
-let allSockets:WebSocket[]=[]
+let allSockets: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket)
+  allSockets.push(socket);
   userCount = userCount + 1;
   console.log("user connected");
   console.log("user count", userCount);
   socket.on("message", (message) => {
     console.log("message received," + message.toString());
- 
-for(let i=0;i<allSockets.length;i++){
-    const s= allSockets[i]
-    s.send(message.toString() + "sent from the server");
-}
 
-  
-
+  allSockets.forEach(s=>{
+    s.send(message.toString() + "sent from the server"); 
+  })
   });
+  socket.on("disconnect",()=>{
+    allSockets=allSockets.filter(x=>x!socket)
+  })
 });
